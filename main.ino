4f839de28e1c8ddc9bb51117e7c896ab64b05137
sir-rigel <sir.rigel@gmail.com>
2016-07-14 20:43:44 -0500

    And readjusted to add replay button

--- main.ino
+++ main.ino
@@ -1,27 +1,30 @@
 // This #include statement was automatically added by the Particle IDE.
 #include "morse.h"
-String replay;
-int button=D1;
+String playback;
+int button=D0;
 int LED=D6;
-bool toggle=FALSE;
+bool play =false;
 void setup() {
  pinMode(button,INPUT);
  pinMode(LED, OUTPUT);
  pinMode(D7, OUTPUT);
  Particle.function("morse", webMorse);
+ Particle.function("replay",replay);
  webMorse("Online");
  digitalWrite(LED,LOW);
 }
 
 int webMorse(String text){
-  if(text==""){
-    return morse(LED,replay);
-  }
-  replay=text;
-  return morse(LED,text);
+  play=true;
+  playback=text;
+  return 1;
+}
+int replay(String text){
+  play=true;
 }
 void loop() {
-  if(digitalRead(button)==LOW){
-    webMorse("");
+  if(play){
+    morse(LED,playback);
   }
+  play=digitalRead(button)==LOW;
 }
